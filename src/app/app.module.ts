42bb632ac6ef22fa96ee56a29963f794cbf95767
julia tokmakova <julia_tokmakova00@mail.ru>
2020-03-11 22:36:55 +0300

    init: start styleguide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColorsComponent } from './components/colors/colors.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
